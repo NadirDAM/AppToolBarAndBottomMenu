b13124cbc2fce2abc4ff1fd3d26101a731f776f8
NadirDAM <nadir.elhaidouri@institutvidreres.cat>
2024-12-01 14:12:30 +0100

    Add submenu

--- app/src/main/java/com/example/apptoolbarandbottommenu/MainActivity.kt
+++ app/src/main/java/com/example/apptoolbarandbottommenu/MainActivity.kt
@@ -5,16 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
@@ -25,42 +20,26 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toolbar: Toolbar
     private lateinit var appBarConfig: AppBarConfiguration
-
+    private var isTransportesExpanded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        //BOTTOM NAVIGATION --
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
 
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNAvigationView)
-
         bottomNavView.setupWithNavController(navController)
 
-
-
-        //NAVIGATION DRAWER BASIC
         val navView = findViewById<NavigationView>(R.id.nav_view)
         navView.setupWithNavController(navController)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        toolbar = findViewById(R.id.toolbar)
 
-
-
-
-        //NAVIGATION DRAWER AMB ACTIONBAR
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        toolbar=findViewById(R.id.toolbar)
-
-
-
-        //The given Activity will be linked to the specified DrawerLayout and
-        // its Actionbar's Up button will be set to a custom drawable.
-        //This drawable shows a Hamburger icon when drawer is closed and an arrow when drawer is open.
-        // It animates between these two states as the drawer opens.
         val toggle = ActionBarDrawerToggle(
             this, drawerLayout, R.string.nav_open, R.string.nav_close
         )
@@ -68,20 +47,27 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
 
         appBarConfig = AppBarConfiguration.Builder(R.id.primerFragment2)
-            //appBarConfig = AppBarConfiguration.Builder(R.id.fragmentInici, R.id.fragmentFinal, R.id.boatFragment)
             .setDrawerLayout(drawerLayout)
             .build()
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig)
 
-        setupActionBarWithNavController(this, navController, appBarConfig)
+        val transportesItem = navView.menu.findItem(R.id.nav_transportlent)
+        val cotxeItem = navView.menu.findItem(R.id.segonFragment2)
+        val vaixellItem = navView.menu.findItem(R.id.tercerFragment2)
 
+
+        transportesItem.setOnMenuItemClickListener {
+            isTransportesExpanded = !isTransportesExpanded
+            cotxeItem.isVisible = isTransportesExpanded
+            vaixellItem.isVisible = isTransportesExpanded
+            true
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        // replace navigation up button with nav drawer button when on start destination
         return NavigationUI.navigateUp(navController, appBarConfig)
-
     }
 }
